c055d1ce558e78625f981e903576608958014140
Davide Ciminaghi <ciminaghi@gnudd.com>
2018-11-09 00:29:03 +0100

    XTENSA: introduce functions for masking/unmasking irqs

--- arch-xtensa/arch/bathos-arch.h
+++ arch-xtensa/arch/bathos-arch.h
@@ -1,6 +1,7 @@
 #ifndef __ARM_BATHOS_ARCH_H__
 #define __ARM_BATHOS_ARCH_H__
 
+#include <stdint.h>
 #include <eagle_soc.h>
 
 #define PROGMEM
@@ -15,6 +16,39 @@
 
 #define interrupt_restore(a)						\
 	__asm__ __volatile__("wsr %0,ps; isync" :: "a" (a) : "memory")
+
+static inline unsigned get_intenable(void)
+{
+	uint32_t intenable;
+
+	__asm__ __volatile__("rsr.intenable %0" : "=r"(intenable));
+	return intenable;
+}
+
+static inline void set_intenable(unsigned v)
+{
+	__asm__ __volatile__("wsr.intenable %0" :: "r"(v));
+}
+
+static inline void mask_irq(int n)
+{
+	uint32_t m = 1 << n;
+	unsigned v;
+
+	interrupt_disable(v);
+	set_intenable(get_intenable() & ~m);
+	interrupt_restore(v);
+}
+
+static inline void unmask_irq(int n)
+{
+	uint32_t m = 1 << n;
+	unsigned v;
+
+	interrupt_disable(v);
+	set_intenable(get_intenable() | m);
+	interrupt_restore(v);
+}
 
 #define __isr __attribute__((section(".isr")))
 
