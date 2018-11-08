0348fcfdc44a42d78085f84a18f2112b28651504
Davide Ciminaghi <ciminaghi@gnudd.com>
2018-11-09 00:32:36 +0100

    XTENSA, EAGLE: introduce hw.h file

---
+++ arch-xtensa/mach-eagle/mach/hw.h
@@ -0,0 +1,13 @@
+#ifndef __XTENSA_EAGLE_HW_H__
+#define __XTENSA_EAGLE_HW_H__
+
+#include <arch/hw.h>
+
+/* VIRTUAL IRQ NUMBERS (demuxed) */
+
+#define SPI0_IRQN 4
+#define SPI1_IRQN 5
+#define SPI2_IRQN 6
+
+#endif /* __XTENSA_EAGLE_HW_H__ */
+
