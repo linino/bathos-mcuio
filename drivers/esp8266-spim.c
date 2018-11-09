7aaac94fe8946435782bba1d4916d3f7ba045f2d
Davide Ciminaghi <ciminaghi@gnudd.com>
2018-11-09 12:59:31 +0100

    drivers/esp8266-spim: add some useful defines.

--- drivers/esp8266-spim.c
+++ drivers/esp8266-spim.c
@@ -12,9 +12,47 @@
 #include <bathos/allocator.h>
 #include <bathos/esp8266-spim.h>
 #include <bathos/buffer_queue_server.h>
+#include <bathos/io.h>
 #include <ets_sys.h>
 #include <osapi.h>
 #include <user_interface.h>
+
+/* Registers' offset */
+#define SPI_CMD		0x00
+#  define SPI_BUSY	BIT(18)
+
+#define SPI_ADDR	0x04
+#define SPI_CTRL	0x08
+#define SPI_RD_STATUS	0x10
+#define SPI_CTRL2	0x14
+#define SPI_CLOCK	0x18
+#  define SPI_CLOCK_PRESCALER_SHIFT 18
+#  define SPI_CLOCK_DIVIDER_SHIFT 12
+#define SPI_USER	0x1c
+#  define COMMAND_EN	BIT(31)
+#  define ADDR_EN	BIT(30)
+#  define DUMMY_EN	BIT(29)
+#  define MISO_EN	BIT(28)
+#  define MOSI_EN	BIT(27)
+#  define DUMMY_IDLE	BIT(26)
+#  define MOSIH		BIT(25)
+#  define MISOH		BIT(24)
+#define SPI_USER1	0x20
+#  define MOSI_BITLEN_MASK 0x1ff
+#  define MOSI_BITLEN_SHIFT 17
+#  define MISO_BITLEN_MASK 0x1ff
+#  define MISO_BITLEN_SHIFT 8
+
+#define SPI_USER2	0x24
+#define SPI_PIN		0x2c
+#define SPI_SLAVE	0x30
+#  define SYNC_RESET	BIT(31)
+#  define SLAVE_MODE	BIT(30)
+#  define WR_RD_BUF_EN	BIT(29)
+#  define WR_RD_STA_EN  BIT(28)
+#  define ALL_INTERRUPTS 0x3e0
+
+#define SPI_W(j)	(0x40 + (j))
 
 struct esp8266_spim_priv {
 	struct bathos_pipe *pipe;
