c814600cf8ef8f0791d386bc9676eaabf2d2920c
Davide Ciminaghi <ciminaghi@gnudd.com>
2018-12-27 14:37:44 +0100

    ATMEGA: add vector.c file

---
+++ arch-atmega/vector.c
@@ -0,0 +1,27 @@
+
+#include <avr/interrupt.h>
+#include <bathos/bathos.h>
+#include <bathos/event.h>
+
+#ifndef VECNO
+#error "Please compile this file with VECNO defined"
+#endif
+
+/*
+ * Single vector table entry
+ */
+static void __attribute__((used, naked, noreturn,
+			   section(xstr(xcat(.vectors.,VECNO)))))
+xcat(__jump_,VECNO)(void)
+{
+	__asm__ __volatile__ ("rjmp " xstr(xcat(__vector_, VECNO)) ::);
+}
+
+/*
+ * Default ISR handler, just returns
+ */
+void __attribute__((naked, noreturn, weak, section(".text.ISR")))
+xcat(__vector_,VECNO)(void)
+{
+	__asm__ __volatile__ ("reti"::);
+}
