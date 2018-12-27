7c3c61a17ff8da07d8b9c001bec90af2afc5982d
Davide Ciminaghi <ciminaghi@gnudd.com>
2018-12-27 14:25:41 +0100

    string.h: when building for avr, use avr-libc prototypes for string functions

--- include/bathos/string.h
+++ include/bathos/string.h
@@ -1,4 +1,7 @@
+#include <generated/autoconf.h>
 #include <arch/bathos-arch.h>
+
+#if !defined CONFIG_ARCH_ATMEGA
 
 extern char *strcpy(char * d, char *s);
 extern int strlen(char *s);
@@ -6,9 +9,10 @@ extern int strnlen(char *s, int count);
 extern int strcmp(const char *s1, const char *s2);
 extern int strncmp(const char *s1, const char *s2, int n);
 extern void *memcpy(void *d, const void *s, int count);
+extern void *memset(void *d, int c, int count);
+
+#endif
 
 #ifndef ARCH_IS_HARVARD
 #define memcpy_p memcpy
 #endif
-
-extern void *memset(void *d, int c, int count);
