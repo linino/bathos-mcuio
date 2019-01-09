5906f7e6499bd3f281ade687a46ae8ff994d771e
Davide Ciminaghi <ciminaghi@gnudd.com>
2019-01-09 10:40:16 +0100

    lib: add glue code for lwip based tcp servers

---
+++ include/bathos/tcp-server-lwip-raw.h
@@ -0,0 +1,116 @@
+#ifndef __TCP_SERVER_LWIP_RAW_H__
+#define __TCP_SERVER_LWIP_RAW_H__
+
+#ifdef CONFIG_HAVE_LWIP
+
+#include "lwip/opt.h"
+#include "lwip/debug.h"
+#include "lwip/stats.h"
+#include "lwip/tcp.h"
+
+#endif /* CONFIG_HAVE_LWIP */
+
+#ifdef LWIP_TCP
+
+/* Forward decl */
+struct tcp_server_socket_lwip_raw;
+
+/* Connection state */
+enum tcp_conn_state
+{
+	ES_FREE = 0,
+	ES_NONE,
+	ES_CONNECTED,
+	ES_CLOSING
+};
+
+/* struct representing a tcp connection */
+struct tcp_conn_data
+{
+	enum tcp_conn_state state;
+	struct tcp_server_socket_lwip_raw *raw_socket;
+	struct tcp_pcb *pcb;
+	uint16_t written;
+	uint16_t acknowledged;
+	int must_close;
+	/* Server private data belonging to this connection */
+	void *priv;
+};
+
+
+struct tcp_server_socket_lwip_raw_ops {
+	/* Returns 0 if OK, -1 if ERROR */
+	int (*accept)(struct tcp_conn_data *);
+	/* Returns number of stored bytes or -1 if not enough memory */
+	int (*recv)(struct tcp_conn_data *,
+		    const void *buf,
+		    unsigned int len);
+	/* Invoked on bytes sent */
+	int (*sent)(struct tcp_conn_data *);
+	/*
+	 * Returns 0 if we can close connection, !0 if we still have stuff
+	 * to send
+	 */
+	int (*poll)(struct tcp_conn_data *);
+	/* Invoked on actual connection close */
+	void (*closed)(struct tcp_conn_data *);
+};
+
+struct tcp_server_socket_lwip_raw {
+	const struct tcp_server_socket_lwip_raw_ops *ops;
+	void (*netif_idle)(struct netif *);
+	struct netif *netif;
+	struct tcp_pcb *tcp_conn_pcb;
+	void *server_priv;
+};
+
+int tcp_server_socket_lwip_raw_init(struct tcp_server_socket_lwip_raw *,
+				    unsigned short port);
+
+int tcp_server_socket_lwip_raw_send(struct tcp_conn_data *,
+				    const void *buf, unsigned int len);
+
+int tcp_server_socket_lwip_raw_close(struct tcp_conn_data *);
+
+int tcp_server_socket_lwip_raw_abort(struct tcp_conn_data *);
+
+int tcp_server_socket_lwip_raw_fini(struct tcp_server_socket_lwip_raw *);
+
+#else /* !LWIP_TCP */
+
+struct tcp_server_socket_lwip_raw;
+struct tcp_conn_data;
+
+static inline int
+tcp_server_socket_lwip_raw_init(struct tcp_server_socket_lwip_raw *r,
+				unsigned short port)
+{
+	return -1;
+}
+
+static inline int tcp_server_socket_lwip_raw_send(struct tcp_conn_data *d,
+						  const void *buf,
+						  unsigned int len)
+{
+	return -1;
+}
+
+static inline int tcp_server_socket_lwip_raw_close(struct tcp_conn_data *d)
+{
+	return -1;
+}
+
+static inline int tcp_server_socket_lwip_raw_abort(struct tcp_conn_data *d)
+{
+	return -1;
+}
+
+static inline int
+tcp_server_socket_lwip_raw_fini(struct tcp_server_socket_lwip_raw *r)
+{
+	return -1;
+}
+
+#endif /* LWIP_TCP */
+
+#endif /* __TCP_SERVER_LWIP_RAW_H__ */
---
+++ lib/tcp-server-lwip-raw.c
@@ -0,0 +1,313 @@
+/*
+ * Raw tcp/ip server using lwip
+ * This code comes from libdfu with minor modifications
+ *
+ * Copyright (c) DogHunter LLC and the Linino organization 2019
+ * Author Davide Ciminaghi 2017 2019
+ */
+#include <bathos/bathos.h>
+#include <bathos/stdio.h>
+#include <generated/autoconf.h>
+#include <bathos/tcp-server-lwip-raw.h>
+
+
+#ifdef LWIP_TCP
+
+#ifndef CONFIG_MAX_TCP_SERVER_CONNECTIONS
+#define MAX_CONNECTIONS 4
+#else
+#define MAX_CONNECTIONS CONFIG_MAX_TCP_SERVER_CONNECTIONS
+#endif
+
+static struct tcp_conn_data connections[MAX_CONNECTIONS];
+
+static struct tcp_conn_data *alloc_connection(void)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(connections); i++)
+		if (connections[i].state == ES_FREE) {
+			connections[i].state = ES_NONE;
+			connections[i].must_close = 0;
+			pr_debug("%s: connection %p allocated\n", __func__,
+				 &connections[i]);
+			return &connections[i];
+		}
+	return NULL;
+}
+
+static void free_connection(struct tcp_conn_data *c)
+{
+	pr_debug("%s: freeing connection %p\n", __func__, c);
+	c->state = ES_FREE;
+}
+
+static void tcp_conn_free(struct tcp_conn_data *es)
+{
+	free_connection(es);
+}
+
+static void tcp_conn_close(struct tcp_pcb *tpcb, struct tcp_conn_data *es)
+{
+	struct tcp_server_socket_lwip_raw *r = es->raw_socket;
+
+	if (tcp_close(tpcb) != ERR_OK) {
+		/*
+		 * http://lwip.wikia.com/wiki/Raw/TCP, tcp_close:
+		 * The function may
+		 * return ERR_MEM if no memory was available for closing the
+		 * connection. If so, the application should wait and try
+		 * again either by using the acknowledgment callback or the
+		 * polling functionality. If the close succeeds, the function
+		 * returns ERR_OK.
+		 */
+		es->must_close = 1;
+		pr_debug("%s: must_close = 1\n", __func__);
+		return;
+	}
+	pr_debug("%s: must_close = 0\n", __func__);
+	es->must_close = 0;
+	tcp_arg(tpcb, NULL);
+	tcp_sent(tpcb, NULL);
+	tcp_recv(tpcb, NULL);
+	tcp_err(tpcb, NULL);
+	tcp_poll(tpcb, NULL, 0);
+	if (r->ops->closed)
+		r->ops->closed(es);
+	tcp_conn_free(es);
+}
+
+err_t tcp_conn_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err)
+{
+	struct tcp_conn_data *es;
+	struct tcp_server_socket_lwip_raw *r;
+	struct pbuf *ptr;
+	int stat = 0;
+
+	LWIP_ASSERT("arg != NULL", arg);
+	es = (struct tcp_conn_data *)arg;
+	r = es->raw_socket;
+	LWIP_ASSERT("raw_socket != NULL", r);
+
+	if (err != ERR_OK) {
+		if (p)
+			/* cleanup, for unkown reason */
+			pbuf_free(p);
+		return err;
+	}
+
+	if (!p) {
+		/* remote host closed connection */
+		es->state = ES_CLOSING;
+		if (r->ops->poll)
+			stat = r->ops->poll(es);
+		if(!stat)
+			/* we're done sending, close it */
+			tcp_conn_close(tpcb, es);
+		return ERR_OK;
+	}
+
+	for (ptr = p; ; ptr = ptr->next) {
+		tcp_recved(es->pcb, ptr->len);
+		if (r->ops->recv) {
+			stat = r->ops->recv(es, ptr->payload, ptr->len);
+			if (stat < 0)
+				break;
+		}
+		if (ptr->tot_len == ptr->len)
+			break;
+	}
+	pbuf_free(p);
+	return ERR_OK;
+}
+
+void tcp_conn_error(void *arg, err_t err)
+{
+	struct tcp_conn_data *es;
+
+	LWIP_UNUSED_ARG(err);
+
+	es = (struct tcp_conn_data *)arg;
+	pr_debug("%s\n", __func__);
+
+	tcp_conn_free(es);
+}
+
+static err_t tcp_conn_poll(void *arg, struct tcp_pcb *tpcb)
+{
+	struct tcp_conn_data *es;
+	struct tcp_server_socket_lwip_raw *r;
+
+	es = (struct tcp_conn_data *)arg;
+	if (!es || !es->raw_socket) {
+		/* nothing to be done */
+		tcp_abort(tpcb);
+		return ERR_ABRT;
+	}
+
+	r = es->raw_socket;
+	if (es->must_close || !r->ops->poll || !r->ops->poll(es))
+		tcp_conn_close(tpcb, es);
+
+	return ERR_OK;
+}
+
+static err_t tcp_conn_sent(void *arg, struct tcp_pcb *tpcb, u16_t len)
+{
+	struct tcp_conn_data *es;
+	struct tcp_server_socket_lwip_raw *r;
+
+	LWIP_UNUSED_ARG(len);
+
+	es = arg;
+	es->acknowledged += len;
+	pr_debug("%s: written = %u, acknowledged = %u\n", __func__,
+		 es->written, es->acknowledged);
+	r = es->raw_socket;
+	if (r->ops->sent)
+		r->ops->sent(es);
+	return ERR_OK;
+}
+
+static err_t tcp_conn_accept(void *arg, struct tcp_pcb *newpcb, err_t err)
+{
+	struct tcp_conn_data *es;
+	struct tcp_server_socket_lwip_raw *r = arg;
+	int stat = 0;
+
+	if ((err != ERR_OK) || (newpcb == NULL) || !r)
+		return ERR_VAL;
+	/*
+	 * Unless this pcb should have NORMAL priority, set its priority now.
+	 * When running out of pcbs, low priority pcbs can be aborted to create
+	 * new pcbs of higher priority.
+	 */
+	tcp_setprio(newpcb, TCP_PRIO_MIN);
+
+	es = alloc_connection();
+	if (!es)
+		return ERR_MEM;
+	es->state = ES_CONNECTED;
+	es->pcb = newpcb;
+	es->raw_socket = r;
+	es->acknowledged = es->written = 0;
+	if (r->ops->accept)
+		stat = r->ops->accept(es);
+	if (stat) {
+		free_connection(es);
+		return ERR_MEM;
+	}
+	/* pass newly allocated es to our callbacks */
+	tcp_arg(newpcb, es);
+	tcp_recv(newpcb, tcp_conn_recv);
+	tcp_err(newpcb, tcp_conn_error);
+	tcp_poll(newpcb, tcp_conn_poll, 0);
+	tcp_sent(newpcb, tcp_conn_sent);
+	tcp_nagle_disable(newpcb);
+	return ERR_OK;
+}
+
+int tcp_server_socket_lwip_raw_init(struct tcp_server_socket_lwip_raw *r,
+				    unsigned short port)
+{
+	err_t err;
+
+	if (!r->ops) {
+		printf("%s: ops is NULL\n", __func__);
+		return -1;
+	}
+	r->tcp_conn_pcb = tcp_new();
+	if (!r->tcp_conn_pcb) {
+		printf("%s: tcp_new() returns error\n", __func__);
+		return -1;
+	}
+	tcp_arg(r->tcp_conn_pcb, r);
+	pr_debug("%s: binding to port %u\n", __func__, port);
+	err = tcp_bind(r->tcp_conn_pcb, IP_ADDR_ANY, port);
+	if (err != ERR_OK) {
+		printf("%s: tcp_bind() to port %u returns error %d\n", __func__, port, err);
+		return -1;
+	}
+	r->tcp_conn_pcb = tcp_listen(r->tcp_conn_pcb);
+	if (!r->tcp_conn_pcb) {
+		printf("%s: tcp_listen() returns NULL\n", __func__);
+		return -1;
+	}
+	tcp_accept(r->tcp_conn_pcb, tcp_conn_accept);
+	return 0;
+}
+
+int tcp_server_socket_lwip_raw_fini(struct tcp_server_socket_lwip_raw *r)
+{
+	if (!r->tcp_conn_pcb) {
+		printf("%s: no socket\n", __func__);
+		return -1;
+	}
+	tcp_arg(r->tcp_conn_pcb, NULL);
+	tcp_abort(r->tcp_conn_pcb);
+	return 0;
+}
+
+extern void my_lwip_idle(void);
+
+int tcp_server_socket_lwip_raw_send(struct tcp_conn_data *es,
+				    const void *buf, unsigned int len)
+{
+	err_t stat;
+	uint16_t l;
+	const char *ptr = buf;
+
+	es->written = 0;
+	do {
+
+		l = min(len - es->written, tcp_sndbuf(es->pcb));
+		if (!l) {
+			pr_debug("%s: no more space in output buffer\n",
+				 __func__);
+			return es->written;
+		}
+		pr_debug("%s: tcp_write(%p, len = %u, written = %d, l = %d)\n",
+			 __func__, &ptr[es->written], len, es->written, l);
+		stat = tcp_write(es->pcb, &ptr[es->written], l,
+				 TCP_WRITE_FLAG_COPY);
+		if (stat != ERR_OK) {
+			printf("%s: tcp_write returned error\n", __func__);
+			break;
+		}
+		es->written += l;
+		pr_debug("%s: written = %d\n", __func__, es->written);
+		if (es->written >= len)
+			break;
+		stat = tcp_output(es->pcb);
+	} while(stat == ERR_OK);
+
+	return stat == ERR_OK ? es->written : -1;
+}
+
+int tcp_server_socket_lwip_raw_close(struct tcp_conn_data *es)
+{
+	pr_debug("%s\n", __func__);
+	tcp_conn_close(es->pcb, es);
+	return 0;
+}
+
+/* Like close, but should be synchronous */
+int tcp_server_socket_lwip_raw_abort(struct tcp_conn_data *es)
+{
+	struct tcp_server_socket_lwip_raw *r = es->raw_socket;
+	struct tcp_pcb *tpcb = es->pcb;
+
+	tcp_abort(tpcb);
+	es->must_close = 0;
+	tcp_arg(tpcb, NULL);
+	tcp_sent(tpcb, NULL);
+	tcp_recv(tpcb, NULL);
+	tcp_err(tpcb, NULL);
+	tcp_poll(tpcb, NULL, 0);
+	if (r->ops->closed)
+		r->ops->closed(es);
+	tcp_conn_free(es);
+	return 0;
+}
+
+#endif /* LWIP_TCP */
