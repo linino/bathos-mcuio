2378e0005b0b0d6ff7930be4c4bd39d2247061ac
Davide Ciminaghi <ciminaghi@gnudd.com>
2019-02-26 11:00:04 +0100

    MQTT: code style, indentation, 80 cols ....
    .h comments to be reviewed, get rid of doxygen stuff.

--- include/bathos/mqtt_pal.h
+++ include/bathos/mqtt_pal.h
@@ -2,9 +2,9 @@
 #define __MQTT_PAL_H__
 
 /*
-MIT License
+  MIT License
 
-Copyright(c) 2018 Liam Bindle
+  Copyright(c) 2018 Liam Bindle
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files(the "Software"), to deal
@@ -29,38 +29,42 @@ SOFTWARE.
  * @file
  * @brief Includes/supports the types/calls required by the MQTT-C client.
  * 
- * @note This is the \em only file included in mqtt.h, and mqtt.c. It is therefore 
+ * @note This is the only file included in mqtt.h, and mqtt.c. It is therefore 
  *       responsible for including/supporting all the required types and calls. 
  * 
  * @defgroup pal Platform abstraction layer
- * @brief Documentation of the types and calls required to port MQTT-C to a new platform.
+ * @brief Documentation of the types and calls required to port MQTT-C to a
+ *        new platform.
  * 
- * mqtt_pal.h is the \em only header file included in mqtt.c. Therefore, to port MQTT-C to a 
- * new platform the following types, functions, constants, and macros must be defined in 
+ * mqtt_pal.h is the only header file included in mqtt.c. Therefore, to port
+ * MQTT-C to a  new platform the following types, functions, constants, and
+ * macros must be defined in 
  * mqtt_pal.h:
  *  - Types:
- *      - \c size_t, \c ssize_t
- *      - \c uint8_t, \c uint16_t, \c uint32_t
- *      - \c va_list
- *      - \c mqtt_pal_time_t : return type of \c MQTT_PAL_TIME() 
- *      - \c mqtt_pal_mutex_t : type of the argument that is passed to \c MQTT_PAL_MUTEX_LOCK and 
- *        \c MQTT_PAL_MUTEX_RELEASE
+ *      - size_t, ssize_t
+ *      - uint8_t, uint16_t, uint32_t
+ *      - va_list
+ *      - mqtt_pal_time_t : return type of MQTT_PAL_TIME() 
+ *      - mqtt_pal_mutex_t : type of the argument that is passed to
+ *        MQTT_PAL_MUTEX_LOCK and MQTT_PAL_MUTEX_RELEASE
  *  - Functions:
- *      - \c memcpy, \c strlen
- *      - \c va_start, \c va_arg, \c va_end
+ *      - memcpy, strlen
+ *      - va_start, va_arg, va_end
  *  - Constants:
- *      - \c INT_MIN
+ *      - INT_MIN
  * 
  * Additionally, three macro's are required:
- *  - \c MQTT_PAL_HTONS(s) : host-to-network endian conversion for uint16_t.
- *  - \c MQTT_PAL_NTOHS(s) : network-to-host endian conversion for uint16_t.
- *  - \c MQTT_PAL_TIME()   : returns [type: \c mqtt_pal_time_t] current time in seconds. 
- *  - \c MQTT_PAL_MUTEX_LOCK(mtx_pointer) : macro that locks the mutex pointed to by \c mtx_pointer.
- *  - \c MQTT_PAL_MUTEX_RELEASE(mtx_pointer) : macro that unlocks the mutex pointed to by 
- *    \c mtx_pointer.
+ *  - MQTT_PAL_HTONS(s) : host-to-network endian conversion for uint16_t.
+ *  - MQTT_PAL_NTOHS(s) : network-to-host endian conversion for uint16_t.
+ *  - MQTT_PAL_TIME()   : returns [type: mqtt_pal_time_t] current time in
+ *                        seconds. 
+ *  - MQTT_PAL_MUTEX_LOCK(mtx_pointer) : macro that locks the mutex pointed
+ *                                       to by \c mtx_pointer.
+ *  - MQTT_PAL_MUTEX_RELEASE(mtx_pointer) : macro that unlocks the mutex
+ *                                          pointed to by mtx_pointer.
  * 
- * Lastly, \ref mqtt_pal_sendall and \ref mqtt_pal_recvall, must be implemented in mqtt_pal.c 
- * for sending and receiving data using the platforms socket calls.
+ * Lastly, mqtt_pal_sendall and mqtt_pal_recvall, must be implemented in
+ * mqtt_pal.c for sending and receiving data using the platforms socket calls.
  */
 
 #include <stdint.h>
@@ -112,12 +116,13 @@ static inline uint16_t MQTT_PAL_HTONS(uint16_t n)
  * 
  * @param[in] fd The file-descriptor (or handle) of the socket.
  * @param[in] buf A pointer to the first byte in the buffer to send.
- * @param[in] len The number of bytes to send (starting at \p buf).
+ * @param[in] len The number of bytes to send (starting at buf).
  * @param[in] flags Flags which are passed to the underlying socket.
  * 
- * @returns The number of bytes sent if successful, an \ref MQTTErrors otherwise.
+ * @returns The number of bytes sent if successful, an MQTTErrors otherwise.
  */
-ssize_t mqtt_pal_sendall(mqtt_pal_socket_handle fd, const void* buf, size_t len, int flags);
+ssize_t mqtt_pal_sendall(mqtt_pal_socket_handle fd, const void* buf,
+			 size_t len, int flags);
 
 /**
  * @brief Non-blocking receive all the byte available.
@@ -125,11 +130,12 @@ ssize_t mqtt_pal_sendall(mqtt_pal_socket_handle fd, const void* buf, size_t len,
  * 
  * @param[in] fd The file-descriptor (or handle) of the socket.
  * @param[in] buf A pointer to the receive buffer.
- * @param[in] bufsz The max number of bytes that can be put into \p buf.
+ * @param[in] bufsz The max number of bytes that can be put into buf.
  * @param[in] flags Flags which are passed to the underlying socket.
  * 
- * @returns The number of bytes received if successful, an \ref MQTTErrors otherwise.
+ * @returns The number of bytes received if successful, an MQTTErrors otherwise.
  */
-ssize_t mqtt_pal_recvall(mqtt_pal_socket_handle fd, void* buf, size_t bufsz, int flags);
+ssize_t mqtt_pal_recvall(mqtt_pal_socket_handle fd, void* buf, size_t bufsz,
+			 int flags);
 
 #endif
